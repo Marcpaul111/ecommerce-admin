b3b8e11a51936fdd29c17665f66fbeb2eaf25548
Marco Lazaro <lazaromarco1526@gmail.com>
2024-11-05 09:22:04 +0800

    new update

--- utils/cors.ts
+++ utils/cors.ts
@@ -1,19 +1,21 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-
 export function middleware(req: NextRequest) {
   console.log(req.nextUrl);
 
   const response = NextResponse.next();
-  
-  response.headers.append("ALLOW-ACCESS-CONTROL-ORIGIN", '*')
+
+  response.headers.append("ALLOW-ACCESS-CONTROL-ORIGIN", "*");
   response.headers.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   response.headers.append("Access-Control-Allow-Headers", "Content-Type");
-  return response;
 
-  
+  // Handle preflight `OPTIONS` request directly
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { headers: response.headers });
+  }
+  return response;
 }
 
 export const config = {
-  matcher: ['/api/:path*'],
-}
+  matcher: ["/api/:path*"],
+};
