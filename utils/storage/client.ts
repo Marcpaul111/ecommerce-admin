96f9a17e380fea9273e4664cf9a5062d4de633df
Marco Lazaro <85611604+Marcpaul111@users.noreply.github.com>
2025-01-02 11:37:30 +0800

    Update client.ts

--- utils/storage/client.ts
+++ utils/storage/client.ts
@@ -38,7 +38,7 @@ export async function uploadImage({file, bucket, folder}: UploadProps){
         return {imageUrl: "", error: "Image Upload Failed"}
     }
 
-    const imageUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/object/public/${bucket}/${data?.path}`
+    const imageUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/s3/object/public/${bucket}/${data?.path}`
 
     return {imageUrl, error: ""}
 }
